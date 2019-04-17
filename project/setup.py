5dca774356e173406c7955be158a6546ce9af29f
Tzu-Hua Kao <tzu-hua.kao@campus.tu-berlin.de>
2019-04-17 20:35:59 +0200

    add python project file

---
+++ project/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+
+# Learn more: https://github.com/kennethreitz/setup.py
+
+from setuptools import setup, find_packages
+
+
+with open('README.rst') as f:
+    readme = f.read()
+
+with open('LICENSE') as f:
+    license = f.read()
+
+setup(
+    name='src',
+    version='1.0',
+    description='Source code for my project',
+    long_description=readme,
+    author='Tzu-Hua Kao',
+    author_email='tzu-hua.kao@campus.tu-berlin.de',
+    url='https://github.com/KaoCircle/anomalyDetection',
+    license=license,
+    packages=find_packages(exclude=('tests', 'docs')),  #same as name
+    #install_requires=['bar', 'greek'], #external packages as dependencies
+    #scripts=[
+    #         'scripts/cool',
+    #         'scripts/skype',
+    #        ]
+    #ref:https://stackoverflow.com/questions/1471994/what-is-setup-py
+)
